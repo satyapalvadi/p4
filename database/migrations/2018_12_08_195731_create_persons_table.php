33f43ff2e64d389ee5ace6ad20b6e12a30ece7ca
Satya Palvadi <satyapalvadi@gmail.com>
2018-12-08 15:03:00 -0500

    first migrations

---
+++ database/migrations/2018_12_08_195731_create_persons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->unsignedDecimal('height');
+            $table->unsignedDecimal('weight');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persons');
+    }
+}
