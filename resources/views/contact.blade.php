832b07697eb39182a11165ad2bd5144fc488956b
Satya Palvadi <satyapalvadi@gmail.com>
2018-12-16 01:42:07 -0500

    removed contact page

--- resources/views/contact.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-    <h1>Contact</h1>
-    <p>
-        For information or help, please email {{ config('mail.supportEmail') }}.
-    </p>
-@endsection
